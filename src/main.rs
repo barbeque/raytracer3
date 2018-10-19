68fc102f65df1ac06d92c31f7daf24d10f8492ee
mike stedman <barbeque@users.noreply.github.com>
2018-10-19 10:16:47 -0600

    With vector math lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate cgmath;
+use cgmath::Vector3;
+
 fn main() {
     let nx : i32 = 200;
     let ny : i32 = 100;
@@ -5,13 +8,12 @@ fn main() {
     println!("P3\n{} {}\n255", nx, ny);
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r : f32 = i as f32 / nx as f32;
-            let g : f32 = j as f32 / ny as f32;
-            let b : f32 = 0.2;
 
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let col = Vector3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+
+            let ir = (255.99 * col.x) as i32;
+            let ig = (255.99 * col.y) as i32;
+            let ib = (255.99 * col.z) as i32;
 
             println!("{} {} {}", ir, ig, ib);
         }
