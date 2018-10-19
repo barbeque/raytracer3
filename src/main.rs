5f3947f9b4cd669a73ba6a2829bdf254fd7a0da2
mike stedman <barbeque@users.noreply.github.com>
2018-10-19 12:39:43 -0600

    Ghetto hardcoded sphere

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use cgmath::*;
 mod ray;
 use ray::{Ray};
 
+fn hit_sphere(centre : Vector3<f32>, radius : f32, ray : &Ray) -> bool {
+    let oc = ray.origin - centre;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * oc.dot(ray.direction);
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    (discriminant > 0.0)
+}
+
 pub fn lerp_v(t : f32, start : Vector3<f32>, end : Vector3<f32>) -> Vector3<f32> {
     assert!(t <= 1.0);
     assert!(t >= 0.0);
@@ -12,6 +21,10 @@ pub fn lerp_v(t : f32, start : Vector3<f32>, end : Vector3<f32>) -> Vector3<f32>
 }
 
 fn colour(r: &Ray) -> Vector3<f32> {
+    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vector3::new(1.0, 0.0, 0.0) // pixels of the sphere
+    }
+
     let unit_direction = r.direction.normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     // lerp blue depending on y-coordinate
